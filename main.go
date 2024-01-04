868f7c9842ddef729baa07f4445ddf6f0adab8fc
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-04 22:21:51 +0000

    modify bitCreate

--- main.go
+++ main.go
@@ -16,7 +16,15 @@ func bitView(c echo.Context) error {
 }
 
 func bitCreate(c echo.Context) error {
-	return c.String(http.StatusOK, "Creating bit")
+	if c.Request().Method != http.MethodPost {
+		return c.String(http.StatusMethodNotAllowed, "Method Not Allowed")
+	}
+
+	m := "Creating bit"
+	if err := c.Bind(&m); err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
+	}
+	return c.String(http.StatusOK, m)
 }
 
 func main() {
@@ -24,7 +32,7 @@ func main() {
 
 	e.GET("/", home)
 	e.GET("/bitbox/view", bitView)
-	e.GET("/bitbox/create", bitCreate)
+	e.POST("/bitbox/create", bitCreate)
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
