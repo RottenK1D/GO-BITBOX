475768b114c03f69930b5eb318f6d75368250668
Rinards Sondors <rinards.sondors@gmail.com>
2024-01-06 23:00:23 +0000

    refactor

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-// home is a simple HTTP handler function which writes a byte slice
+// Home is a simple HTTP handler function which writes a byte slice
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
@@ -16,7 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from BITBOX"))
 }
 
-// bitView displays a specific bit based on ID provided in the URL
+// BitView displays a specific bit based on ID provided in the URL
 func bitView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
@@ -27,7 +27,7 @@ func bitView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific bit with ID %d...", id)
 }
 
-// bitCreate creates a new bit
+// BitCreate creates a new bit
 func bitCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,9 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/RottenK1D/GO-BITBOX/ui/html/pages"
+	"github.com/a-h/templ"
 )
 
 func main() {
@@ -11,6 +14,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/bit/view", bitView)
 	mux.HandleFunc("/bit/create", bitCreate)
+	mux.Handle("/hello", templ.Handler(pages.Hello()))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
